250eb502c87bcd20acb0a75d80b9fa1cd786ec35
Mario Skrlec <mario.skrlec@tildeloop.com>
2024-06-05 09:57:27 +0200

    Added check to see if app directory exists which I cannot believe I forgot

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
 #!/usr/bin/env node
 
-import {intro, outro, group, text, confirm} from '@clack/prompts';
-import {create} from "./index.js";
-import kleur from "kleur";
+import { intro, outro, group, text, confirm } from '@clack/prompts';
+import { create } from './index.js';
+import kleur from 'kleur';
+import fs from 'fs';
 
 async function run() {
     intro(`Welcome to Creatif CLI project scaffolding`);
 
     const g = await group(
         {
-            appDirectory: () => text({
-                message: 'What is your app directory?',
-                placeholder: 'Leave blank for the current directory'
-            }),
-            projectName: () => text({
-                message: 'What is your project name?',
-                placeholder: 'Leave blank and app directory will be chosen',
-                validate(value) {
-                    if (value.length < 1 && value.length > 200) return `Project name must have between 1 and 200 characters.`;
-                },
-            }),
-            hasStarterProject: () => confirm({
-                message: 'Would you like to setup a starter project? (you can delete it later)',
-            }),
+            appDirectory: () =>
+                text({
+                    message: 'What is your app directory?',
+                    placeholder: 'Leave blank for the current directory',
+                    validate(value) {
+                        try {
+                            const exists = fs.existsSync(value);
+                            if (exists) {
+                                return `Directory with name '${value}' already exists.`;
+                            }
+                        } catch (e) {
+                            if (e instanceof Error) {
+                                return `Could not determine if the app directory '${value}' exists. This is the error: ${e.message}`;
+                            }
+
+                            return `Could not determine if the app directory '${value}' exists. Please, choose another directory name.`;
+                        }
+                    },
+                }),
+            projectName: () =>
+                text({
+                    message: 'What is your project name?',
+                    placeholder: 'Leave blank and app directory will be chosen',
+                    validate(value) {
+                        if (value.length < 1 && value.length > 200)
+                            return `Project name must have between 1 and 200 characters.`;
+                    },
+                }),
+            hasStarterProject: () =>
+                confirm({
+                    message: 'Would you like to setup a starter project? (you can delete it later)',
+                }),
         },
         {
             onCancel: () => {
                 process.exit(0);
             },
-        }
+        },
     );
 
     await create({
